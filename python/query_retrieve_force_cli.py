c2e770605419f6e3588c82aa16fddce6e4dec053
Dstrasel <siruku@gmail.com>
2016-12-22 10:02:54 -0500

    Added function comments and and refactored function logic to be more semantic.

--- python/query_retrieve_force_cli.py
+++ python/query_retrieve_force_cli.py
@@ -21,27 +21,33 @@ import time
 import os
 import csv
 import subprocess
+import sys
+
 
 def import_queries(file_path):
-    secrets_filename = file_path
+    # Parses each line of the ./inputs/queries.csv file and executes them in OS shell using force-cli
+    query_file_path = file_path
     query_list = {}
-    with open(secrets_filename, 'r') as file:
+    with open(query_file_path, 'r') as file:
         query_list = csv.DictReader(file, delimiter=',')
-        return [command.run_query(query['query']) for query in query_list]
+        return [command.run_query(sql_statement['query']) for sql_statement in query_list]
+
 
 def run_tests(accounts_path, tests_path):
-    print(">>>read_credentials: " + accounts_path)
-    secrets_filename = accounts_path
+    # Parses each line of .inputs/accounts.csv, logs into using force-cli and executes each test case
+    # print(">>>read_credentials: " + accounts_path)
+    tests_file_path = accounts_path
     account_list = {}
-    with open(secrets_filename, 'r') as file:
+    with open(tests_file_path, 'r') as file:
         account_list = csv.DictReader(file, delimiter=',')
         for account in account_list:
             command.login(account['username'], account['pw'])
             command.run_queries(tests_path)
-            break
+            break  # Stops the loop from processing every account; just for testing
 
 
 def send_email(user, pwd, recipient, subject, body):
+    # Sends an email using passed in arguments, catches errors in try/except
     print(">>>send_email: " + "To: " + recipient + " From: " + user)
     import smtplib
 
@@ -68,33 +74,40 @@ def send_email(user, pwd, recipient, subject, body):
 
 
 class command:
-
+    """This class file serves as a force-cli wrapper library for Python. This will execute each command using the
+    subprocess Python module."""
     def __init__(self):
+
         """Do stuff which will login and create an active session with Salesforce?"""
         pass
 
     def execute(command):
-        """Static method."""
+        """This method will execute the string statement passed in."""
         output = None
         try:
             output = subprocess.check_output(command)
         except subprocess.CalledProcessError as e:
             output = e.output
-        print(output.decode())
         return output.decode()
 
     def login(username, password):
+        """Used to pass a login to force-cli using username, password arguments."""
         return command.execute("force login -u=%s -p=%s" % (username, password))
 
     def run_query(sql_statement):
+        """Will execute a single SOQL statement."""
         return command.execute("force query " + sql_statement)
 
     def run_queries(file_path):
+        """Will execute n amount of SOQL statements based on parsed query file."""
         query_filename = file_path
         query_list = {}
         with open(query_filename, 'r') as file:
             query_list = csv.DictReader(file, delimiter=',')
             return [command.run_query(query['query']) for query in query_list]
+
+    def output_command(self, command):
+        pass
 
 
         """
@@ -111,14 +124,21 @@ class log:
 
 
     def __init__(self, filename):
+        """This uses a passed in 'filename' argument to create a log file with the same name."""
         self.filename = filename
 
+    def open_file(self, filename):
+        """TODO: write 'with open' code, to reduce redundant open calls in following methods"""
+        pass
+
     def write(self, text):
+        "Writes a single line to specified filename. Can refer to preexisting log files."
         with open(self.filename, 'w', newline=' ') as csvfile:
             log_file = csv.writer(csvfile, delimiter=',', quoting=csv.QUOTE_ALL)
             log_file.writerow(text)
 
-    def write_log(self, fields):
+    def write_csv(self, fields):
+        """Will write to a csv file """
         with open(self.filename, 'w') as csvfile:
             fieldnames = list(fields[0].keys())
             writer = csv.DictWriter(csvfile, fieldnames=fieldnames, delimiter=',', quoting=csv.QUOTE_ALL, lineterminator='\n')
@@ -134,4 +154,16 @@ class log:
 python_output_query_file = log('../outputs/python_output_query_file.csv')
 
 dict_list = [{'first_name': 'Baked', 'last_name': 'Beans'}, {'first_name': 'Lovely', 'last_name': 'Spam'}, {'first_name': 'Wonderful', 'last_name': 'Spam'}]
-python_output_query_file.write_log(dict_list)
+# python_output_query_file.write_log(dict_list)
+
+print(command.login("mxw_cmx@preventure.com","cmxforc3"))
+print(command.run_query("SELECT COUNT(SFDCID__C) Active FROM Contact where mxw__Is_Active__c = true"))
+
+def main():
+    pass
+    # If no args are specified, print "Nothing specified" and return pass
+    # If args specified, pass them into "run_tests", arg[0] should be account path and arg[1] should be test path,
+    # while arg[2] can flag whether emails should be sent. Default False, unless explicitly passed.
+
+if __name__ == "__main__":
+    main()
